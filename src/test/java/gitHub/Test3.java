a62fa23daed2867aacabdf3e0973309d2afa762e
Bhushan <bhushandada30@gamil.com>
2024-06-13 23:04:47 +0530

    commiting the new changes of Test3 class

--- src/test/java/gitHub/Test3.java
+++ src/test/java/gitHub/Test3.java
@@ -4,7 +4,7 @@ public class Test3 {
 
 	public static void main(String args[]) {
 		
-		System.out.println("This is 3rd testing class");
+		System.out.println("This is 3rd testing class,testing purpose");
 	}
 	
 
