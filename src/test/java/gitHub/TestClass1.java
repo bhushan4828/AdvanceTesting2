ad5cc12b59d94cc21b48b7e4f257041465ee8b45
Bhushan <bhushandada30@gamil.com>
2024-06-14 23:10:01 +0530

    Changes made to test class1

--- src/test/java/gitHub/TestClass1.java
+++ src/test/java/gitHub/TestClass1.java
@@ -4,8 +4,9 @@ public class TestClass1 {
 
 	
  public void method1() {
+	int a=6; 
 	 
-	 System.out.println("This is test class,testing purpose");
+	 System.out.println("This is test class,testing purpose,Can u please test it?");
 	 
  }
 	
